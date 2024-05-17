97b63b1f1bc68c1c58a9d20ae5341cbaf5e40588
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2024-05-17 17:24:07 +0200

    Enable dev logging to see requests

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -2,9 +2,14 @@ package server;
 
 import io.javalin.Javalin;
 public class Server {
+
+    private static final int port = 9443;
     public static void main(String[] args) {
-        var app = Javalin.create(/*config*/)
+        var app = Javalin.create(conf -> {
+                    conf.bundledPlugins.enableDevLogging();
+                    conf.jetty.defaultPort = port;
+            })
                 .get("/", ctx -> ctx.result("Hello World"))
-                .start(9443);
+                .start(port);
     }
 }
